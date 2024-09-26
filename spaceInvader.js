3257d561c464d75c8fd9238fba0ee3748e2b8988
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-09-26 22:23:20 +0200

    Milstone 1 Space Invader Tokken

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,13 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+context.fillStyle = "#000000";
+context.fillRect(0, 0, 300, 300)
+
+context.fillStyle = "#D957C4"
+context.fillRect(75, 25, 150, 100)
+context.fillRect(125, 125, 50, 100)
+context.fillRect(75, 175, 50, 100)
+context.fillRect(175, 175, 50, 100)
