af1654f1d4708ea3548cdaba622828e9b88b06a4
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-10-10 10:58:40 +0200

    Space Invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -3,11 +3,15 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext("2d");
 
-context.fillStyle = "#000000";
-context.fillRect(0, 0, 300, 300)
+signature();
 
-context.fillStyle = "#D957C4"
-context.fillRect(75, 25, 150, 100)
-context.fillRect(125, 125, 50, 100)
-context.fillRect(75, 175, 50, 100)
-context.fillRect(175, 175, 50, 100)
+function signature() {
+	context.fillStyle = "#000000";
+	context.fillRect(0, 0, 300, 300);
+
+	context.fillStyle = "#D957C4";
+	context.fillRect(75, 25, 150, 100);
+	context.fillRect(125, 125, 50, 100);
+	context.fillRect(75, 175, 50, 100);
+	context.fillRect(175, 175, 50, 100);
+}
