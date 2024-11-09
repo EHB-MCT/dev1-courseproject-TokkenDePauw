5d9cf10d1649694a5293d8c3940fdb7249789ace
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-11-09 20:05:51 +0100

    generativeDesign heart monitor

--- tasks/generativeDesign.js
+++ tasks/generativeDesign.js
@@ -9,117 +9,81 @@ let height = context.canvas.height;
 background();
 
 function background() {
-	context.fillStyle = "black";
+	context.fillStyle = "#000000";
 	context.fillRect(0, 0, width, height);
 }
 
-stars();
-
-function stars() {
-	let stars = 200;
-
-	for (let i = 0; i < stars; i++) {
-		let x = Utils.randomNumber(0, width);
-		let y = Utils.randomNumber(0, height);
-		let r = Utils.randomNumber(0, 2);
-
-		context.fillStyle = "white";
-		Utils.fillCircle(x, y, r);
+perlin();
+// Adapted by Peter Dickx for the DEV1 course @ Erasmushogeschool Brussel
+function perlin() {
+	for (let i = 0; i < width; i++) {
+		let n = noise.perlinNoise(i / 75) * 300 + height / 3;
+		context.fillStyle = "#43F51B";
+		context.fillRect(i, n, 5, 5);
 	}
 }
 
-sun();
+drawLines();
 
-function sun() {
-	let sunX = width - 100;
-	let SunY = 100;
-	let sunR = 75;
+function drawLines() {
+	context.strokeStyle = "#43F51B";
+	context.lineWidth = 0.25;
+	let x = width / 2;
+	let y = height / 2;
 
-	context.fillStyle = "#FF8000";
-	Utils.fillCircle(sunX, SunY, sunR + 10);
-	context.fillStyle = "#FFDD00";
-	Utils.fillCircle(sunX, SunY, sunR);
-}
+	for (let i = 0; i < width / 20; i++) {
+		Utils.drawLine(x, y, i * 20, 0);
+		Utils.drawLine(x, y, i * 20, height);
+	}
 
-planets();
-
-function planets() {
-	let planets = 15;
-
-	for (let i = 0; i < planets; i++) {
-		let x = Utils.randomNumber(0, width);
-		let y = Utils.randomNumber(0, height);
-		let r = Utils.randomNumber(5, 20);
-		let h = i * 24;
-		let s = Utils.randomNumber(40, 60);
-		let l = Utils.randomNumber(40, 50);
-
-		context.fillStyle = Utils.hsl(h, s, l);
-		Utils.fillCircle(x, y, r);
+	context.strokeStyle = "#43F51B";
+	for (let i = 0; i < height / 20; i++) {
+		Utils.drawLine(x, y, 0, i * 20);
+		Utils.drawLine(x, y, width, i * 20);
 	}
 }
 
-rocket();
+bpm();
 
-function rocket() {
-	let rocketX = Utils.randomNumber(0, width);
-	let rocketY = Utils.randomNumber(0, height);
-	let rocketWidth = 50;
-	let rocketHeight = 100;
+function bpm() {
+	let randomNumber = Math.random();
 
-	// Bovenste deel van de raket
-	context.fillStyle = "#D32F2F";
-	Utils.fillEllipse(rocketX + 25, rocketY, rocketWidth / 2, rocketHeight / 4);
-
-	// Motor van de raket
-	context.fillStyle = "#262626";
-	Utils.fillEllipse(rocketX + 25, rocketY + 125, rocketWidth, rocketHeight / 2);
-	context.fillStyle = "#000000";
-	context.fillRect(rocketX - 25, rocketY + 125, 100, 50);
-
-	// Middenste deel van de raket
-	context.fillStyle = "#B0B0B0";
-	context.fillRect(rocketX, rocketY, rocketWidth, rocketHeight);
-
-	// Raam van de raket
-	context.fillStyle = "#4A4A4A";
-	Utils.fillCircle(rocketX + 25, rocketY + 50, 15);
-	context.fillStyle = "#ADD8E6";
-	Utils.fillCircle(rocketX + 25, rocketY + 50, 10);
-
-	// Onderste deel van de raket
-	context.fillStyle = "#4A4A4A";
-	context.fillRect(rocketX, rocketY + 100, rocketWidth, rocketHeight / 4);
-
-	// Vuur van de raket
-	context.fillStyle = "#FF4500";
-	Utils.fillCircle(rocketX + 25, rocketY + 210, 20, 20);
-	context.fillStyle = "#FFA500";
-	Utils.fillCircle(rocketX + 25, rocketY + 180, 20, 20);
-	context.fillStyle = "#700";
-	Utils.fillCircle(rocketX + 25, rocketY + 150, 20, 20);
-}
-
-ufo();
-
-function ufo() {
-	let ufos = 3;
-	for (let i = 0; i < ufos; i++) {
-		let ufoX = Utils.randomNumber(0, width);
-		let ufoY = Utils.randomNumber(0, height);
-		let ufoR = 25;
-		let uforX = 25;
-		let uforY = 10;
-
-		context.fillStyle = "#ADD8E6";
-		Utils.fillCircle(ufoX, ufoY + -10, ufoR - 10);
+	if (randomNumber < 0.33) {
+		context.fillStyle = "#FF0000";
+	} else if (randomNumber < 0.66) {
+		context.fillStyle = "#FFFF00";
+	} else {
 		context.fillStyle = "#00FF00";
-		Utils.fillCircle(ufoX, ufoY - 15, ufoR - 20);
-		context.fillStyle = "black";
-		Utils.fillCircle(ufoX - 2, ufoY - 15, ufoR - 24);
-		context.fillStyle = "black";
-		Utils.fillCircle(ufoX + 2, ufoY - 15, ufoR - 24);
-		context.fillStyle = "#808080";
-		Utils.fillEllipse(ufoX, ufoY, uforX, uforY);
 	}
+
+	//B
+	context.fillRect(width - 400, 50, 10, 110);
+	context.fillRect(width - 360, 60, 10, 40);
+	context.fillRect(width - 360, 110, 10, 40);
+	context.fillRect(width - 400, 50, 40, 10);
+	context.fillRect(width - 400, 100, 40, 10);
+	context.fillRect(width - 400, 150, 40, 10);
+	//P
+	context.fillRect(width - 325, 50, 10, 110);
+	context.fillRect(width - 285, 60, 10, 40);
+	context.fillRect(width - 325, 50, 40, 10);
+	context.fillRect(width - 325, 100, 40, 10);
+	//M
+	context.fillRect(width - 250, 60, 10, 100);
+	context.fillRect(width - 240, 50, 20, 10);
+	context.fillRect(width - 220, 60, 10, 50);
+	context.fillRect(width - 210, 50, 20, 10);
+	context.fillRect(width - 190, 60, 10, 100);
+	//6
+	context.fillRect(width - 150, 60, 10, 90);
+	context.fillRect(width - 140, 50, 30, 10);
+	context.fillRect(width - 150, 100, 40, 10);
+	context.fillRect(width - 140, 150, 30, 10);
+	context.fillRect(width - 110, 110, 10, 40);
+	//9
+	context.fillRect(width - 40, 60, 10, 90);
+	context.fillRect(width - 70, 50, 30, 10);
+	context.fillRect(width - 70, 100, 40, 10);
+	context.fillRect(width - 70, 150, 30, 10);
+	context.fillRect(width - 80, 60, 10, 40);
 }
