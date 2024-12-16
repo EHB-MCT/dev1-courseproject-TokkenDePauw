5ec16d754d61c17f58821cba4d910f41c2bc0af0
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-12-16 20:42:57 +0100

    context.js updated

--- scripts/context.js
+++ scripts/context.js
@@ -1,3 +1,4 @@
+"use strict";
 /** @type {CanvasRenderingContext2D} */
 let context;
 
@@ -6,8 +7,15 @@ setupCanvasContext();
 export default context;
 
 function setupCanvasContext() {
+	//get a reference to the canvas element and configure it to fill the whole window
 	let canvas = document.querySelector("canvas");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	context = canvas.getContext("2d");
+}
+
+window.onresize = reload;
+
+function reload() {
+	location.reload();
 }
