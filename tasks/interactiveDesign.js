2511acf4c83a21f7d27b6736dedcb75b696fc6c4
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-12-17 15:13:26 +0100

    creates  raindrops

--- tasks/interactiveDesign.js
+++ tasks/interactiveDesign.js
@@ -11,3 +11,17 @@ let sat = 0;
 
 setup();
 update();
+
+function setup() {
+	window.onmousemove = mouseMove;
+}
+
+createRaindrop();
+
+function createRaindrop() {
+	x: Utils.randomNumber(0, width);
+	y: Utils.randomNumber(0, height);
+	size: Utils.randomNumber(2, 7);
+	speed: Utils.randomNumber(5, 10);
+	opacity: Utils.randomNumber(0.6, 1);
+}
