98c10df7bfe3501306e35d78ba457421a6bc020d
Tokken De Pauw <tokken.de.pauw@studebt.ehb.be>
2024-12-18 13:26:05 +0100

    Ik heb de regendruppelcode die ik eerder schreef gecombineerd met mijn generative design, wat zorgt voor een toffe achtergrondanimatie.

--- tasks/interactiveDesign.js
+++ tasks/interactiveDesign.js
@@ -6,34 +6,77 @@ import * as noise from "../scripts/noise.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-background();
+let raindrops = [];
+let wind = 0;
+let sat = 0;
 
-function background() {
-	context.fillStyle = "#000000";
+setup();
+update();
+
+function setup() {
+	window.onmousemove = mouseMove;
+
+	for (let i = 0; i < 500; i++) {
+		raindrops.push(createRaindrop());
+	}
+}
+
+function createRaindrop() {
+	return {
+		x: Utils.randomNumber(0, width),
+		y: Utils.randomNumber(0, height),
+		size: Utils.randomNumber(2, 7),
+		speed: Utils.randomNumber(5, 10),
+		opacity: Utils.randomNumber(0.6, 1),
+	};
+}
+
+function drawRaindrop(x, y, size) {
+	context.beginPath();
+	context.moveTo(x, y);
+	context.lineTo(x, y + size);
+	context.strokeStyle = Utils.hsla(200, 100, 100 - (sat / height) * 100, 50);
+	context.lineWidth = 2;
+	context.stroke();
+}
+
+function update() {
+	context.fillStyle = Utils.hsl(200, 50, 20);
 	context.fillRect(0, 0, width, height);
-}
 
-drawLines(20);
+	for (let i = 0; i < raindrops.length; i++) {
+		let drop = raindrops[i];
 
-function drawLines(lineScaping) {
-	context.strokeStyle = "#43F51B";
-	context.lineWidth = 0.25;
-	let x = width / 2;
-	let y = height / 2;
+		if (drop.y > height) {
+			drop.y = -drop.size;
+			drop.x = Utils.randomNumber(0, width);
+		}
 
-	for (let i = 0; i < width / lineScaping; i++) {
-		Utils.drawLine(x, y, i * lineScaping, 0);
-		Utils.drawLine(x, y, i * lineScaping, height);
+		drop.y += drop.speed;
+		drop.x += Utils.randomNumber(-1, 1) + wind;
+
+		drawRaindrop(drop.x, drop.y, drop.size);
 	}
 
-	context.strokeStyle = "#43F51B";
-	for (let i = 0; i < height / lineScaping; i++) {
-		Utils.drawLine(x, y, 0, i * lineScaping);
-		Utils.drawLine(x, y, width, i * lineScaping);
-	}
+	perlin();
+
+	bpm();
+
+	drawSignature();
+
+	requestAnimationFrame(update);
 }
 
-perlin();
+/**
+ *
+ * @param {MouseEvent} eventData
+ */
+function mouseMove(eventData) {
+	let xOffset = width / 2 - eventData.pageX;
+	wind = xOffset / 100;
+	sat = eventData.pageY / 2;
+}
+
 // Adapted by Peter Dickx for the DEV1 course @ Erasmushogeschool Brussel
 function perlin() {
 	for (let i = 0; i < width; i++) {
@@ -43,18 +86,8 @@ function perlin() {
 	}
 }
 
-bpm();
-
 function bpm() {
-	let randomNumber = Math.random();
-
-	if (randomNumber < 0.33) {
-		context.fillStyle = "#FF0000";
-	} else if (randomNumber < 0.66) {
-		context.fillStyle = "#FFFF00";
-	} else {
-		context.fillStyle = "#00FF00";
-	}
+	context.fillStyle = "#00FF00";
 
 	//B
 	context.fillRect(width - 400, 50, 10, 110);
@@ -86,4 +119,15 @@ function bpm() {
 	context.fillRect(width - 70, 100, 40, 10);
 	context.fillRect(width - 70, 150, 30, 10);
 	context.fillRect(width - 80, 60, 10, 40);
+}
+
+function drawSignature() {
+	context.fillStyle = "#000000";
+	context.fillRect(width - 300, height - 300, 300, 300);
+
+	context.fillStyle = "#D957C4";
+	context.fillRect(width - 225, height - 275, 150, 100);
+	context.fillRect(width - 175, height - 175, 50, 100);
+	context.fillRect(width - 225, height - 125, 50, 100);
+	context.fillRect(width - 125, height - 125, 50, 100);
 }
